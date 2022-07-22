4263987f3cca24b23ab5cade11c1c13c10fddf05
iqbalalayubbi <79147766+iqbalalayubbi@users.noreply.github.com>
2022-07-22 21:18:37 +0700

    menambahkan teman

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const name = ["iqbal","akbar","dimas"];
+const name = ["iqbal","akbar","dimas","bayu","danu"];
 
 // bikin folder views untuk file ejs 
 app.set('view engine','ejs');
