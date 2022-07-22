732e432453448b36b254205096599d5b1739d2c2
iqbalalayubbi <79147766+iqbalalayubbi@users.noreply.github.com>
2022-07-22 20:48:12 +0700

    template starter nodejs and express

---
+++ script.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// bikin folder views untuk file ejs 
+app.set('view engine','ejs');
+
+// bikin folder public di root bisa include cssimage dll
+app.use(express.static('public'));
+
+app.get('/',(req,res) => {
+    res.render('login',{title:'login'})
+})
+app.get('/home',(req,res) => {
+    res.render('home',{title:'home'})
+})
+
+app.listen(port,() => {
+    console.log(`server is running on port ${port}`)
+})
