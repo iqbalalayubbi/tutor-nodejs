1c007108d54b8671d5dcc8c779596d7350b12bae
iqbalalayyubi <iqbal.is415@gmail.com>
2022-07-22 21:38:19 +0700

    menghapus nama teman

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const name = ["iqbal","akbar","dimas","bayu","danu"];
-
 // bikin folder views untuk file ejs 
 app.set('view engine','ejs');
 
